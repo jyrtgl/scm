afc3d56714d390452d0a1e059e91e407a85c052c
jyrtgl <jayartagle999@gmail.com>
2022-06-06 12:54:54 +0800

    Update
    update

--- functions.php
+++ functions.php
@@ -169,7 +169,7 @@ function login(){
 
 				$_SESSION['user'] = $logged_in_user;
 				$_SESSION['success']  = "You are now logged in";
-				header('location: admin/home.php');		  
+				header('location: admin/admin_home.php');		  
 			}elseif ($logged_in_user['user_type'] == 'staff') {
 
 				$_SESSION['user'] = $logged_in_user;
